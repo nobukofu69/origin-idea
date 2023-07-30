36fe2bddd2c8905950e64162c46f253612ee7c57
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-07-30 18:59:51 +0900

    ストロングパラメータ記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
