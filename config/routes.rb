aaf6f177619a19ae2ec62d76bb0313cf3e53c63c
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-01 00:13:48 +0900

    showアクション&コンサルタントフラグ機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      patch :toggle_consultant_status
+    end
+  end
 end
