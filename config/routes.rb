6cf50ca78d46c708857949692281d6721c20a174
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-04 11:22:50 +0900

    ルーティングにコメント追記

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :users, only: [:show, :edit, :update] do
     member do
+      # ユーザーのコンサルタントステータスを変更する
       patch :toggle_consultant_status
+
+      # ユーザーが受けたコンサル依頼一覧を表示する
+      get :received_consultations
     end
 
     resources :consultations, only: [:new, :create]
