ffb608021d16c29ef804057e34d7727e92a7d8c5
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-06 01:15:35 +0900

    indexメソッドのロジックを追記

--- app/controllers/talkrooms_controller.rb
+++ app/controllers/talkrooms_controller.rb
@@ -1,5 +1,7 @@
+# app/controllers/talkrooms_controller.rb
 class TalkroomsController < ApplicationController
   def index
+    @consultations = Consultation.includes(:requester, :consultant).where(status: [2, 3])
   end
 
   def show
