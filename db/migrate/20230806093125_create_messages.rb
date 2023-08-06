06ea9e150dc52295b4cdef67eb6b1dbe1e96af3d
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-06 19:08:40 +0900

    テーブル構成を変更

--- db/migrate/20230806093125_create_messages.rb
+++ db/migrate/20230806093125_create_messages.rb
@@ -3,9 +3,8 @@ class CreateMessages < ActiveRecord::Migration[7.0]
     create_table :messages do |t|
       t.references :sender_id, null: false, foreign_key: { to_table: :users }
       t.references :consultation_id, null: false, foreign_key: { to_table: :consultations }
-      t.text :content
-      t.boolean :is_read
-
+      t.text :content, null: false
+      t.boolean :is_read, null: false, default: false
       t.timestamps
     end
   end
