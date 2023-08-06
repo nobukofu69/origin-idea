425ebe6fc57fb264895a3bacfc879952465314d0
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-06 16:20:42 +0900

    コントローラの記述を一部修正

--- app/controllers/talkrooms_controller.rb
+++ app/controllers/talkrooms_controller.rb
@@ -2,8 +2,10 @@
 class TalkroomsController < ApplicationController
   def index
     @consultations = Consultation.includes(:requester, :consultant).where(status: [2, 3])
+      .where("requester_id = ? OR consultant_id = ?", current_user.id, current_user.id)
   end
 
   def show
+    @consultation = Consultation.find(params[:consultation_id])
   end
 end
