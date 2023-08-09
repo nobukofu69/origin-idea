e4e126e8ed17a5275a7f9dba114d8951b38e1e2a
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-09 19:53:14 +0900

    enumを新たに追加および変更

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,5 +1,6 @@
 class Consultation < ApplicationRecord
-  enum status: { not_requested: 0, requesting: 1, matching: 2, closed: 3 }
+  enum request_status: { not_requested: 0, requesting: 1, completed: 2}
+  enum talkroom_status: { not_created: 0, opened: 1, closed: 2}
   belongs_to :consultant, class_name: "User"
   belongs_to :requester, class_name: "User"
   has_many :messages
