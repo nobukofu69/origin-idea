28d7f635b50eb19b6824312c0f86a256ef020c7c
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-10 23:56:25 +0900

    ゲストユーザ用の条件を設定

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,16 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      @users = User.where.not(id: current_user.id)
-    else
+    # ユーザーログインしていない場合､全ユーザーを表示するガード節
+    unless user_signed_in?
       @users = User.all
+      return
+    end
+
+    # ユーザーがゲストユーザーの場合､インスタンス変数にゲストユーザーを代入する
+    if current_user.guest_user?
+      @user = current_user
+    else # ユーザーがゲストユーザーでない場合､インスタンス変数に自分以外のユーザーを代入する
+      @users = User.where.not(id: current_user.id)
     end
   end
 end
