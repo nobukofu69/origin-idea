3fa5c6f3526170470e7ce82ee616ae47fd649d42
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-17 22:14:40 +0900

    rubocop関連のgem追加およびビルド

--- Gemfile
+++ Gemfile
@@ -56,7 +56,9 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec', '~> 3.12', group: :development
-  gem 'rubocop', '~> 1.51', require: false
+  gem 'rubocop', require: false
+  gem 'rubocop-performance', require: false
+  gem 'rubocop-rails', require: false
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,56 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_806_093_125) do
-  create_table 'consultations', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'consultant_id', null: false
-    t.bigint 'requester_id', null: false
-    t.text 'request_content', null: false
-    t.datetime 'answer_deadline', null: false
-    t.integer 'request_status', default: 0, null: false
-    t.integer 'talkroom_status', default: 0, null: false
-    t.boolean 'is_read', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['consultant_id'], name: 'index_consultations_on_consultant_id'
-    t.index ['requester_id'], name: 'index_consultations_on_requester_id'
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_093125) do
+  create_table "consultations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "consultant_id", null: false
+    t.bigint "requester_id", null: false
+    t.text "request_content", null: false
+    t.datetime "answer_deadline", null: false
+    t.integer "request_status", default: 0, null: false
+    t.integer "talkroom_status", default: 0, null: false
+    t.boolean "is_read", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["consultant_id"], name: "index_consultations_on_consultant_id"
+    t.index ["requester_id"], name: "index_consultations_on_requester_id"
   end
 
-  create_table 'messages', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'sender_id', null: false
-    t.bigint 'consultation_id', null: false
-    t.text 'content', null: false
-    t.datetime 'sent_at', null: false
-    t.boolean 'is_read', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['consultation_id'], name: 'index_messages_on_consultation_id'
-    t.index ['sender_id'], name: 'index_messages_on_sender_id'
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "sender_id", null: false
+    t.bigint "consultation_id", null: false
+    t.text "content", null: false
+    t.datetime "sent_at", null: false
+    t.boolean "is_read", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["consultation_id"], name: "index_messages_on_consultation_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
   end
 
-  create_table 'users', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'name', null: false
-    t.date 'birthdate'
-    t.integer 'gender', default: 0, null: false
-    t.string 'profession'
-    t.text 'profile'
-    t.string 'profile_image_id'
-    t.text 'skill'
-    t.string 'rating'
-    t.boolean 'is_consultant', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.date "birthdate"
+    t.integer "gender", default: 0, null: false
+    t.string "profession"
+    t.text "profile"
+    t.string "profile_image_id"
+    t.text "skill"
+    t.string "rating"
+    t.boolean "is_consultant", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'consultations', 'users', column: 'consultant_id'
-  add_foreign_key 'consultations', 'users', column: 'requester_id'
-  add_foreign_key 'messages', 'consultations'
-  add_foreign_key 'messages', 'users', column: 'sender_id'
+  add_foreign_key "consultations", "users", column: "consultant_id"
+  add_foreign_key "consultations", "users", column: "requester_id"
+  add_foreign_key "messages", "consultations"
+  add_foreign_key "messages", "users", column: "sender_id"
 end
