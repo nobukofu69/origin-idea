230459bc50a5dcd0ad805a406e31b37a14fe37ab
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-19 21:17:08 +0900

    hide_navbarヘルパーメソッド作成し､application_html.erbに追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,11 @@ module ApplicationHelper
       '未設定'
     end
   end
+
+  def hide_navbar?
+    (controller_name == 'home' && action_name == 'top') ||
+    (controller_path == 'devise/registrations' && action_name == 'new') ||
+    (controller_path == 'devise/sessions' && action_name == 'new')
+  end
+
 end
