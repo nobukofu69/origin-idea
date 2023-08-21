3e2c0d9122bb4ecbe9ceb8bfc73fe90919037b17
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-22 04:35:17 +0900

    コンサル依頼中の判定条件を修正

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -6,25 +6,28 @@ class Consultation < ApplicationRecord
   has_many :messages, dependent: :destroy, inverse_of: :consultation
   validates :request_content, presence: true
 
-  # current_userとuser同士で相談を依頼中､またはトークルームがオープン中の場合に trueを返すクラスメソッド
+  # ログインユーザーが相談を依頼中､またはトークルームがオープン中の場合に trueを返すクラスメソッド
   def self.consulted?(current_user, user)
-    query_string = <<-SQL
-      ((requester_id = :current_user AND consultant_id = :user)
-      OR (requester_id = :user AND consultant_id = :current_user))
-      AND (request_status = :requesting OR talkroom_status = :opened)
-    SQL
+    where(requester: current_user, consultant: user)
+      .exists?(['request_status = ? OR talkroom_status = ?',
+                Consultation.request_statuses[:requesting],
+                Consultation.talkroom_statuses[:opened]])
+  end
 
-    exists?([
-      query_string,
-      current_user: current_user,
-      user: user,
-      requesting: request_statuses[:requesting],
-      opened: talkroom_statuses[:opened]
-    ])
+  # 未読の依頼があればtrueを返すクラスメソッド
+  def self.unread_request?(current_user)
+    where(consultant: current_user, is_read: false).exists?
+  end
+
+  # インスタンスが未読であることを判定及び出力するメソッド
+  def unread_notification
+    is_read ? '' : '未読あり'
   end
 
   # ログインユーザー以外のユーザーを取得する
   def other_user(current_user)
     requester == current_user ? consultant : requester
   end
+
+
 end
