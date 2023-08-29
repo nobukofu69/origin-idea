d2039c44dd91962b3e69f45ef951e7595c33fb35
Nobuyuki Shimoi <nobukofu69@gmail.com>
2023-08-29 11:17:36 +0900

    コメントを一部修正

--- app/channels/talkroom_channel.rb
+++ app/channels/talkroom_channel.rb
@@ -15,7 +15,7 @@ class TalkroomChannel < ApplicationCable::Channel
     message = current_user.senders.create!(sent_at: Time.current, consultation_id: data['consultation_id'],
                                            content: data['content'])
 
-    # Messageモデルをレンダリングしてtalkroom_channelへブロードキャストする
+    # talkroom_channelへブロードキャストする
     ActionCable.server.broadcast "talkroom_channel_#{params[:consultation_id]}", { message: render_message(message) }
   end
 
